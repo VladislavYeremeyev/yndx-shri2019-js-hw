86f7bc8d08b40fc123a424a80133beae15f99691
Vladislav Yeremeyev <vladislav.eremeev@uchi.ru>
2019-09-23 10:30:28 +0300

    README updated, tests added

--- README.md
+++ README.md
@@ -5,3 +5,20 @@
 > *Node version*: v12.7.0+
 
 Необходимо написать полифил для ES6 Promise.
+
+Код полифила описан в файле `polyfill.js`. Протестировал работу, используя [Promises/A+ Compliance Test Suite](https://github.com/promises-aplus/promises-tests).
+
+Для запуска тестов:
+
+```
+node test
+```
+
+## Что сделано
+
+- [x] Promise.prototype.then()
+- [x] Promise.prototype.catch()
+- [x] Promise.resolve()
+- [x] Promise.reject()
+- [x] Обработка thenable объектов
+- [x] Соответствие Promise/A+
--- polyfill.js
+++ polyfill.js
@@ -20,10 +20,9 @@
 					'Promise constructor could not be called as a function'
 				);
 			}
-			this._state = PENDING;
-			this._subscriptions = [];
-			// TODO
-			executeResolver(fn);
+			this.state = PENDING;
+			this.observers = [];
+			executeResolver(fn, this);
 		} else {
 			// template literals = ES6
 			throw new TypeError('Promise resolver ' + fn + ' is not a function');
@@ -42,6 +41,118 @@
 		return false;
 	}
 
+	function handleThenable(promise, x) {
+		try {
+			var then = isThenable(x);
+		} catch (err) {
+			reject(promise, err);
+			return true;
+		}
+
+		if (then) {
+			var resolved = false;
+			try {
+				then(
+					function(ful) {
+						if (resolved) return;
+						resolved = true;
+						fulfill(promise, ful);
+					},
+
+					function(rej) {
+						if (resolved) return;
+						resolved = true;
+						reject(promise, rej);
+					}
+				);
+			} catch (err) {
+				if (!resolved) {
+					reject(promise, err);
+				}
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	function handleObservers(promise, method) {
+		promise.observers.forEach(function(observer) {
+			var value = promise.value;
+			try {
+				if (typeof observer[method] === 'function') {
+					value = observer[method](value);
+				}
+
+				if (observer.newPromise === value) {
+					throw new TypeError(
+						'Returned the same promise'
+					);
+				}
+
+				if (handleThenable(observer.newPromise, value)) {
+					return;
+				}
+
+				if (method === 'onRejected' && typeof observer[method] !== 'function') {
+					reject(observer.newPromise, value);
+				} else {
+					fulfill(observer.newPromise, value);
+				}
+			} catch (err) {
+				reject(observer.newPromise, err);
+			}
+		});
+	}
+
+	function fulfill(promise, x) {
+		// typeof null === 'object', needs to check value
+		if (x && typeof x === 'object' && x.constructor === Promise) {
+			x.then(fulfill.bind(undefined, promise), reject.bind(undefined, promise));
+			return;
+		}
+
+		if (promise.state === PENDING && !promise.value) {
+			if (handleThenable(promise, x)) {
+				return;
+			}
+
+			promise.value = x;
+			promise.state = FULFILLED;
+			handleObservers(promise, 'onFulfilled');
+		}
+	}
+
+	function reject(promise, reason) {
+		if (promise.state === PENDING && !promise.value) {
+			promise.state = REJECTED;
+			promise.value = reason;
+
+			handleObservers(promise, 'onRejected');
+		}
+	}
+
+	function executeResolver(resolver, promise) {
+		function resolvePromise(value) {
+			setTimeout(function() {
+				fulfill(promise, value);
+			}, 0);
+		}
+
+		function rejectPromise(reason) {
+			setTimeout(function() {
+				reject(promise, reason);
+			}, 0);
+		}
+
+		try {
+			resolver(resolvePromise, rejectPromise);
+		} catch (err) {
+			rejectPromise(err);
+		}
+	}
+
 	Promise.resolve = function(value) {
 		if (value && typeof value === 'object' && value.constructor === Promise) {
 			return value;
@@ -58,6 +169,41 @@
 		});
 	};
 
-	// TODO then, catch
+
+	Promise.prototype.then = function(onFulfilled, onRejected) {
+		var observer = {
+			onFulfilled:
+				typeof onFulfilled === 'function' && onFulfilled.bind(undefined),
+			onRejected:
+				typeof onRejected === 'function' && onRejected.bind(undefined),
+			newPromise: new Promise(function() {})
+		};
+		
+		if (this.state === FULFILLED && typeof onFulfilled === 'function') {
+			setTimeout(
+				function() {
+					fulfill(observer.newPromise, onFulfilled(this.value));
+				}.bind(this),
+				0
+			);
+		}
+		if (this.state === REJECTED && typeof onRejected === 'function') {
+			setTimeout(
+				function() {
+					reject(observer.newPromise, onRejected(this.value));
+				}.bind(this),
+				0
+			);
+		} else {
+			this.observers.push(observer);
+		}
+
+		return observer.newPromise;
+	};
+
+	Promise.prototype.catch = function(onRejected) {
+		return this.then(undefined, onRejected);
+	};
+
 	environment.Promise = Promise;
 })(typeof window === 'object' ? window : global);
